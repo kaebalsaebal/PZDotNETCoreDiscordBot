21a4c4414d5f14d5059a3e22040dc3416a148bcd
kaebalsaebal <ssassaium@gmail.com>
2026-06-30 17:41:30 +0900

    fix typo

--- Interactions/SlashCommands/CommandSlashCommands.cs
+++ Interactions/SlashCommands/CommandSlashCommands.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace DotNETCoreDiscordBot
 {
-    [RequireAuthorizedUser]
+    [RequireAuthorizedUser, RequireCommandChannel]
     public class CommandSlashCommands : InteractionModuleBase<SocketInteractionContext>
     {
 
